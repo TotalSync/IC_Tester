f602a4f984e843d91c14f04d61580a257fd911c7
Ian Green <1an6reen@gmail.com>
2021-03-04 11:44:48 -0500

    Expanded Write Function and added more mainline code

--- Arduino/ic_tester/eeprom.ino
+++ Arduino/ic_tester/eeprom.ino
@@ -4,8 +4,7 @@
   Example is for the 23A1024
   8|SPI
   CO--IKX+
-  0x02|400ns|100ns
-  0x
+  0x02|0x000000
   %[Data in Hex]
 
   EOF
@@ -13,9 +12,7 @@
 
   (Number of Pins)|(Serial)   <= Pin Number and Type
   (Pin 1)(Pin 2)(Pin 3) etc.  <= Pin Declaration
-
-  [Hex Number for write instruction (0-F)]|[Programming High Time]|[Programming Low time]
-  [Start Address]
+  [Hex Number for write instruction (0-F)]|[Start Address]
   %[Data to Write]
 
   C: Chip Select
@@ -36,12 +33,16 @@ boolean ProgramEEPROM() {
   if (dataFile) {
     while (dataFile.available()) {
       uint8_t pinnum = (uint8_t) dataFile.readStringUntil('|').c_str();
-      uint8_t pins = new uint8_t[pinnum];// <------ HEAP ALLOCATION
+      uint8_t* pins = new uint8_t[pinnum];// <------ HEAP ALLOCATION
       String promType = dataFile.readStringUntil('\n');
       String promPinData = dataFile.readStringUntil('\n');
-      String buf = dataFile.readStringUntil('|');
-      int promWrite = StringHex_to_int(buf.c_str(), buf.length());
-      int address = 0x0;
+      for (int c = 0; c < promPinData.length(); c++ ){
+        pins[c] = promPinData.c_str()[c]; // May cause issues int the future
+      }
+      String promWriteString = dataFile.readStringUntil('|');
+      int promWrite = StringHex_to_int(promWriteString.c_str(), promWriteString.length());
+      String promAddressStart = dataFile.readStringUntil('\n');
+      int promAddress = StringHex_to_int(promAddressStart.c_str(), promAddressStart.length());
 
 
 
@@ -50,8 +51,14 @@ boolean ProgramEEPROM() {
 
 }
 
+//Works in 8-bit increments.
+//Can only take 2 hex values at a time
+//Ex. 0xAA or 0x0 works but not 0x023
+//Address must shift out 24 bits regardless of whether they are filled or not
 void writeToProm(int value, int address, int promWrite, uint8_t DI, uint8_t CLK) {
-  shiftOut(DI, CLK, MSBFIRST, promWrite); //Works in 8-bit increments. May need bit manip to make full solution.
-  shiftOut(DI, CLK, MSBFIRST, address);
+  shiftOut(DI, CLK, MSBFIRST, promWrite); 
+  for (int i = 2; i >= 0; i--) {
+    shiftOut(DI, CLK, MSBFIRST, (address >> (i * 8)));
+  }
   shiftOut(DI, CLK, MSBFIRST, value);
 }
